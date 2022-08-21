c208087cf54ad506e6f680b5814d952c7588333e
ProgrammerBayzid <mdbayzid2622002@gmail.com>
2022-08-22 01:41:00 +0530

    second commit

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
-const totalPlayerArray[];
+const totalPlayerArray=[];
 
 
 
+function selaectedPlayerDisplay (selaectPlayer){
+    console.log(selaectPlayer);
+    const tabelBody=document.getElementById('palyer-list');
 
+     for(let i = 0; i< selaectPlayer.length ; i++){
+        const name = totalPlayerArray[i].playerNamne;
+        const Rank = totalPlayerArray[i].playerRank;
+        const tr = document.createElement("tr");
+        tr.innerHTML=`
+          <th></th>
+          <td></td>
+          <td></td>
+        `;
+        tabelBody.appendChild(tr);
+     }
+}
 function addToTotalPlayer(element){
     // console.log(element.parentNode.parentNode.children);
     // console.log(element.parentNode.parentNode.children[0].innerText);
     // console.log(element.parentNode.parentNode.children[1].children[0].innerText);
     const playerNamne = element.parentNode.parentNode.children[0].innerText;
     const playerRank = element.parentNode.parentNode.children[1].children[0].innerText;
+ const playerObject ={
+    playerNamne: playerNamne,
+    playerRank: parseFloat(playerRank)
+ }
 
+ totalPlayerArray.push(playerObject);
+ 
+// console.log(totalPlayerArray);
+// console.log(totalPlayerArray.length);
+
+ document.getElementById("added-player").innerText=totalPlayerArray.length;
+
+ display();
 }
