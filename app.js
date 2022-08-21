587f615b0150a4a1657eab2da9defcd854e6fec4
ProgrammerBayzid <mdbayzid2622002@gmail.com>
2022-08-22 00:30:43 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const totalPlayerArray[];
+
+
+
+
+function addToTotalPlayer(element){
+    // console.log(element.parentNode.parentNode.children);
+    // console.log(element.parentNode.parentNode.children[0].innerText);
+    // console.log(element.parentNode.parentNode.children[1].children[0].innerText);
+    const playerNamne = element.parentNode.parentNode.children[0].innerText;
+    const playerRank = element.parentNode.parentNode.children[1].children[0].innerText;
+
+}
