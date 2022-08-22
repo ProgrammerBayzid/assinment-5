541078c36cc481298eb75f63b9e9e8965c21936f
ProgrammerBayzid <mdbayzid2622002@gmail.com>
2022-08-22 10:57:24 +0530

    four commit

--- app.js
+++ app.js
@@ -4,38 +4,74 @@ const totalPlayerArray=[];
 
 function selaectedPlayerDisplay (selaectPlayer){
     console.log(selaectPlayer);
-    const tabelBody=document.getElementById('palyer-list');
+    const tabelBody=document.getElementById("palyer-list");
     tabelBody.innerHTML="";
 
-     for(let i = 0; i< selaectPlayer.length ; i++){
+     for(let i = 0; i < selaectPlayer.length ; i++){
         const name = totalPlayerArray[i].playerNamne;
         const rank = totalPlayerArray[i].playerRank;
         const tr = document.createElement("tr");
-        tr.innerHTML=`
-          <th>${i+1}</th>
-          <td>${name}</td>
-          <td>${rank}</td>
+        tr.innerHTML = `
+        <th>${i+1}</th>
+        <td>${name}</td>
+        <td>${rank}</td>
         `;
         tabelBody.appendChild(tr);
      }
 }
 function addToTotalPlayer(element){
-    // console.log(element.parentNode.parentNode.children);
-    // console.log(element.parentNode.parentNode.children[0].innerText);
-    // console.log(element.parentNode.parentNode.children[1].children[0].innerText);
     const playerNamne = element.parentNode.parentNode.children[0].innerText;
     const playerRank = element.parentNode.parentNode.children[1].children[0].innerText;
- const playerObject ={
+    
+    const playerObject ={
     playerNamne: playerNamne,
-    playerRank: parseFloat(playerRank)
+    playerRank: parseFloat(playerRank),
  }
 
  totalPlayerArray.push(playerObject);
- 
-// console.log(totalPlayerArray);
-// console.log(totalPlayerArray.length);
+ let addedPlayer = document.getElementById("added-player");
+ let addPlayerText= addedPlayer.innerText;
+ addedPlayer.innerText= totalPlayerArray.length;
 
- document.getElementById("added-player").innerText=totalPlayerArray.length;
-
- display();
+ selaectedPlayerDisplay(totalPlayerArray);
 }
+
+
+
+function getValueFromInput(inputId) {
+    const inputField = document.getElementById(inputId);
+    const inputFieldString = inputField.value;
+    const inputFieldNumber = parseFloat(inputFieldString)
+    return inputFieldNumber
+}
+
+function getValueFromText(textId, value) {
+    const textField = document.getElementById(textId);
+    const textFieldString = textField.innerText;
+    const textFieldNumber = parseFloat(textFieldString)
+    textField.innerText = value
+    return textFieldNumber;
+
+}
+function getValueFromTextNum(textId) {
+    const textField = document.getElementById(textId);
+    const textFieldString = textField.innerText;
+    const textFieldNumber = parseFloat(textFieldString)
+    return textFieldNumber;
+}
+
+
+document.getElementById('calculate').addEventListener('click',function(){
+    const selectPlayer = totalPlayerArray.length;
+    const perPlayer = getValueFromInput('Per-Player');
+    let totalPlayerExpenses= perPlayer * selectPlayer;
+    getValueFromText('player-expense', totalPlayerExpenses)
+})
+
+document.getElementById('total-calculate').addEventListener('click',function(){
+    const managerCost = getValueFromInput('manager-cost');
+    const coachCost = getValueFromInput('coach-cost');
+    const totalExpenses = managerCost + coachCost ;
+    getValueFromText('total-expense', totalExpenses)
+})
+
